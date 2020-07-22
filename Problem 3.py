e64030a3d63f0539505d83c7831d293c6a7176ac
actually4NOT <49729056+actually4NOT@users.noreply.github.com>
2020-07-22 23:49:45 +0300

    Initial problem 3

--- Problem 3.py
+++ Problem 3.py
@@ -1,8 +1,13 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
 
 number = 600851475143
 results = []
 for i in range(number):
-    if number % (i+1) == 0:
-        results.append(i+1)
-        print(i+1)
+    if number % (i + 1) == 0:
+        results.append(i + 1)
+        print(i + 1)
 print(max(results))
+
+# Instagram, ProjectEuler.net: ilya._romanovich
+# Telegram: @kizilov_elijah
