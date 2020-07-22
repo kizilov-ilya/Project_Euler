57dbdc1f12c8333f7fa23e863a90ce44c01b9dc7
actually4NOT <49729056+actually4NOT@users.noreply.github.com>
2020-07-22 22:24:05 +0300

    Solve Problem 1

---
+++ Problem 1.py
@@ -0,0 +1,13 @@
+import colorama
+
+
+# Description
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+def problem(n: int):
+    return sum([number for number in range(n) if number % 3 == 0 or number % 5 == 0])
+
+
+print(problem(1000))
