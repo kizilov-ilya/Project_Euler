a3dd7142563099ec37a004beb7c48fc41b675898
actually4NOT <49729056+actually4NOT@users.noreply.github.com>
2020-07-22 23:16:19 +0300

    Verify problem 2

--- Problem 1.py
+++ Problem 1.py
@@ -1,6 +1,3 @@
-import colorama
-
-
 # Description
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 # The sum of these multiples is 23.
@@ -11,3 +8,6 @@ def problem(n: int):
 
 
 print(problem(1000))
+
+# Instagram, ProjectEuler.net: ilya._romanovich
+# Telegram: @kizilov_elijah
